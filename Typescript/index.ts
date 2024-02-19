4d5ce6fe31de3fa5ce37b53e8e50fdb817496206
Dilbar Ali <160045634+Dilbarali54@users.noreply.github.com>
2024-02-20 02:07:56 +0500

    update file

---
+++ Typescript/index.ts
@@ -0,0 +1,151 @@
+// Q2 ANS
+//let person: string("Hello Dilbar ali,would you like to learn some python today?");
+//console.log(person);
+
+// Q3 ANS
+//let personName:string="Sir Zia khan";
+//console.log("Lowercase",personName .toLowerCase());
+//console.log("Upercase",personName .toUpperCase());
+//console.log("Titlecase",personName.replace(/\b\w/g,c=> c.toUpperCase())); 
+
+// Q4 ANS
+//var quote:string="The ultmate aim of the ego is not to see something, but to be something.";
+//console.log(quote);
+//var author:string="Allma Iqbal";
+//console.log(author);
+
+// Q5 ANS
+//let famouse_person:string="Allama Iqbal";
+//let message:string="The ultmate aim of the ego is not to see something, but to be something. "
+//console.log(`${famouse_person} Once Said, ${message}`);
+
+// Q6 ANS
+//const personName:string="  \n\tDilbar Ali\t\n   ";
+//console.log(personName);
+//const lName:string=personName.trim();
+//console.log(lName);
+
+
+// Q7 ANS 
+//let addation:number=5+3;
+//console.log(addation);
+//let substraction:number=11-3;
+//console.log(substraction);
+//let multiply:number=2*4;
+//console.log(multiply);
+//let division:number=16%2;
+//console.log(division);
+
+// Q8 ANS
+//console.log(4+4);
+//console.log(10-2);
+//console.log(2*2*2);
+//console.log(16/2);
+
+// Q9 ANS
+//const favorite_Number:number=7;
+//const message:string='Here is my favorite number ' + favorite_Number;
+//console.log(message);
+
+// Q 10 ANS
+//program written by; Dilbar Ali
+// Current Date: 16-02-2024
+// This program will be Hello world
+//console.log('Hello world');
+
+//program written by; Dilbar Ali
+// Current Date: 16-02-2024
+// This program will add two digits
+//console.log(4+4);
+
+// Q11 ANs
+//let names:string[]=['Dilbar','Subhan','Shahid','Adnan'];
+//for(let i=0; i<names.length;i++)[
+    //console.log(names[i])
+//]
+
+// Q12 ANS
+//let names:string[]=['Dilbar','Subhan','Shahid','Adnan'];
+//const message:string='Well Come to webdevelopment';
+//for(let i=0; i<names.length;i++)[
+    //console.log(message + names[i])
+//]
+
+// Q13 ANS
+//let transporataion:string[]=['motercycle','Car','Aeroplane','Bus'];
+//for(let i=0; i<transporataion.length; i++){
+   // console.log('I would like to own Car' +transporataion[i])
+//}
+
+// Q14 ANS
+// let Guest_List:string[]=['Azhar','Nisar','Bilal','Kahlid'];
+//  for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Mr. ' +Guest_List[i] + ',\n\n It is our pleasure to invite you in our party.\n\nThank You!')
+//  }
+
+// Q18 Ans
+// let places:string[]=['Turkey','Indonesia','Dubia','Effiletower','Times Square'];
+// console.log('orignal ' +places);
+// console.log('copy' +[...places].sort());
+// console.log('orignal ' +places);
+// console.log('copy ' +[...places].sort().reverse());
+// console.log('orignal ' +places);
+// console.log('orignal ' +places.reverse());
+// console.log('orignal ' +places.reverse());
+// console.log('orignal ' +places.sort());
+// console.log('orignal ' +places.sort().reverse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
